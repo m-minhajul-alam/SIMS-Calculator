5ad4cc477ed5292ad044a5397fa0eb44cfd0a23a
Minhajul-alam <alam.minhajul@outlook.com>
2023-08-16 23:19:59 +0600

    added price tex field

--- js/sims-calculator.js
+++ js/sims-calculator.js
@@ -1,28 +1,38 @@
+function theTex(price) {
+    const findText = ((price / 95.5) * 4.5);
+    const theTex = Math.round(findText)
+    return theTex;
+}
+
 function addTex(price) {
     const findText = ((price / 95.5) * 4.5);
     const finalPrice = Math.round(parseFloat(price) + findText);
     return finalPrice;
 }
 
-document.getElementById('add-tax-btn').addEventListener('click', function () {
+document.getElementById('get-tax-btn').addEventListener('click', function () {
+    const priceTax = theTex(document.getElementById('input-num').value)
     const priceWithTax = addTex(document.getElementById('input-num').value)
     if (priceWithTax < 1) {
         alert('Please, Use numbers greater than 0 for tax. Thank you.');
         document.getElementById('input-num').value = '';
         return;
     } else {
+        document.getElementById('num-tax').innerText = priceTax;
         document.getElementById('num-with-tax').innerText = priceWithTax;
     }
 })
 
 document.getElementById('input-num').addEventListener('keyup', function (event) {
     if (event.key === "Enter") {
+        const priceTax = theTex(document.getElementById('input-num').value)
         const priceWithTax = addTex(document.getElementById('input-num').value)
         if (priceWithTax < 1) {
             alert('Please, Use numbers greater than 0 for tax. Thank you.');
             document.getElementById('input-num').value = '';
             return;
         } else {
+            document.getElementById('num-tax').innerText = priceTax;
             document.getElementById('num-with-tax').innerText = priceWithTax;
         }
     }
@@ -30,6 +40,6 @@ document.getElementById('input-num').addEventListener('keyup', function (event) 
 
 document.getElementById('clean-btn').addEventListener('click', function () {
     document.getElementById('input-num').value = '';
+    document.getElementById('num-tax').innerText = '00';
     document.getElementById('num-with-tax').innerText = '00';
-
 })
