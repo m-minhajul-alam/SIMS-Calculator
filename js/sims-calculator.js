6c2b24a3d9c85d5a22ddc5cdc066c5e5c524efb6
Minhajul-alam <alam.minhajul@outlook.com>
2023-08-17 07:07:30 +0600

    change top sheet tax count filed

--- js/sims-calculator.js
+++ js/sims-calculator.js
@@ -4,22 +4,22 @@ function billTex(price) {
     return billTex;
 }
 
-function topSheetTex(price) {
-    const findText = ((price / 100) * 4.5);
-    const topSheetTex = Math.round(findText);
-    return topSheetTex;
-}
-
 function addTex(price) {
     const findText = ((price / 95.5) * 4.5);
     const addTex = Math.round(Number(price) + findText);
     return addTex;
 }
 
+function topSheetTex(price) {
+    const findText = ((price / 100) * 4.5);
+    const topSheetTex = Math.round(findText);
+    return topSheetTex;
+}
+
 document.getElementById('get-tax-btn').addEventListener('click', function () {
     const priceTax = billTex(document.getElementById('input-num').value)
-    const priceTax2 = topSheetTex(document.getElementById('input-num').value)
     const priceWithTax = addTex(document.getElementById('input-num').value)
+    const priceTax2 = topSheetTex(document.getElementById('input-num').value)
     if (priceWithTax < 1) {
         alert('Please, type numbers greater than 0 for tax. Thank you.');
         document.getElementById('input-num').value = '';
@@ -34,8 +34,8 @@ document.getElementById('get-tax-btn').addEventListener('click', function () {
 document.getElementById('input-num').addEventListener('keyup', function (event) {
     if (event.key === "Enter") {
         const priceTax = billTex(document.getElementById('input-num').value)
-        const priceTax2 = topSheetTex(document.getElementById('input-num').value)
         const priceWithTax = addTex(document.getElementById('input-num').value)
+        const priceTax2 = topSheetTex(document.getElementById('input-num').value)
         if (priceWithTax < 1) {
             alert('Please, type numbers greater than 0 for tax. Thank you.');
             document.getElementById('input-num').value = '';
@@ -51,6 +51,6 @@ document.getElementById('input-num').addEventListener('keyup', function (event) 
 document.getElementById('clean-btn').addEventListener('click', function () {
     document.getElementById('input-num').value = '';
     document.getElementById('num-tax').innerText = '00';
-    document.getElementById('num-tax2').innerText = '00';
     document.getElementById('num-with-tax').innerText = '00';
+    document.getElementById('num-tax2').innerText = '00';
 })
